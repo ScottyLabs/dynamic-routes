d74e4a4082b24262cacf48de32bf631e55f338f7
Gram Liu <gram.liu226@gmail.com>
2020-04-20 20:39:26 -0400

    Test fix

--- src/app.js
+++ src/app.js
@@ -10,9 +10,9 @@ const port = process.env.port || 3000;
 const client_secret = process.env.client_secret;
 const client_id = process.env.client_id;
 const redirect_uri = "urn:ietf:wg:oauth:2.0:oob";
-const oAuth2Client = authorize(client_secret, client_id, redirect_uri);
+// const oAuth2Client = authorize(client_secret, client_id, redirect_uri);
 
-routes(oAuth2Client, app);
+// routes(oAuth2Client, app);
 
 app.route('/').get((req, res) => {
   res.send('ScottyLabs Dynamic Routes API');
